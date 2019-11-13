d455a4c99cabead6697084801e8a5f8b6fc3b77d
Poranut Chollavorn <pipech@gmail.com>
2019-11-13 08:02:27 +0000

    feat(desktop): add metabase page to desktop

--- metabase_integration/config/desktop.py
+++ metabase_integration/config/desktop.py
@@ -4,11 +4,22 @@ from frappe import _
 
 def get_data():
 	return [
+		# {
+		# 	"module_name": "Metabase Integration",
+		# 	"color": "grey",
+		# 	"icon": "octicon octicon-file-directory",
+		# 	"type": "module",
+		# 	"label": _("Metabase Integration")
+		# },
 		{
-			"module_name": "Metabase Integration",
-			"color": "grey",
-			"icon": "octicon octicon-file-directory",
-			"type": "module",
-			"label": _("Metabase Integration")
-		}
+			'module_name': 'Metabase Integration',
+			'category': 'Places',
+			'label': _('Metabase Dashboard'),
+			'icon': 'octicon octicon-graph',
+			'type': 'link',
+			'link': '#metabase-dashboard',
+			'color': '#FF4136',
+			'standard': 1,
+			'idx': 11
+		},
 	]
