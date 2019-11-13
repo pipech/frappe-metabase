7a82a2ad5e3cebbbc9d4973440191542b98d14ad
Poranut Chollavorn <pipech@gmail.com>
2019-11-13 08:39:43 +0000

    feat(page): add dashboard selection

--- metabase_integration/metabase_integration/page/metabase_dashboard/metabase_dashboard.js
+++ metabase_integration/metabase_integration/page/metabase_dashboard/metabase_dashboard.js
@@ -1,7 +1,68 @@
-frappe.pages['metabase-dashboard'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'None',
-		single_column: true
+/* eslint require-jsdoc: 0 */
+
+frappe.pages['metabase-dashboard'].on_page_load = (wrapper) => {
+	// init page
+	const page = frappe.ui.make_app_page({
+		'parent': wrapper,
+		'title': 'Dashboard',
+		'single_column': true,
 	});
+
+	new MetabaseDashboard(page, wrapper);
+};
+
+class MetabaseDashboard {
+	constructor(page, wrapper) {
+		this.wrapper = wrapper;
+		this.page = page;
+		this.pageMain = $(page.main);
+		this.pageAction = (
+			$(this.wrapper)
+				.find('div.page-head div.page-actions')
+		);
+		this.pageTitle = $(this.wrapper).find('div.title-text');
+		this.init();
+	}
+
+	init() {
+		this.createSelectionField();
+	}
+
+	createSelectionField() {
+		// create dashboard selection field
+		this.selectionField = frappe.ui.form.make_control({
+			'parent': this.pageAction,
+			'df': {
+				'fieldname': 'Dashboard',
+				'fieldtype': 'Link',
+				'options': 'Metabase Dashboard',
+				'onchange': () => {
+					const dashboardName = this.selectionField.get_value();
+					if (dashboardName) {
+						this.dashboardName = dashboardName;
+						this.changeTitle(dashboardName);
+						// clear input
+						this.selectionField.set_input('');
+					}
+				},
+				'get_query': () => {
+					return {
+						'filters': {
+							'is_active': 1,
+						},
+					};
+				},
+				'placeholder': 'Select Dashboard',
+			},
+			'render_input': true,
+		});
+
+		// change css
+		this.pageAction.removeClass('page-actions');
+		this.selectionField.$wrapper.css('text-align', 'left');
+	}
+
+	changeTitle() {
+		this.pageTitle.text(`${this.dashboardName} Dashboard`);
+	}
 }
