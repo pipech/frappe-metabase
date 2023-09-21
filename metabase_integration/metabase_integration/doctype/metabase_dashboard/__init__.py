8e7cd262771492629adf57edfdc8a660f8f93eae
Ardan <ardan@corebpm.com>
2023-09-21 23:07:37 +0800

    fix code

--- metabase_integration/metabase_integration/doctype/metabase_dashboard/__init__.py
+++ metabase_integration/metabase_integration/doctype/metabase_dashboard/__init__.py
@@ -8,57 +8,74 @@ import time
 
 @frappe.whitelist()
 def get_url(dashboard):
-	# get metabase info
-	metabase_config = frappe.get_single('Metabase Settings')
-	# get dashboard info
-	dashboard = frappe.get_doc('Metabase Dashboard', dashboard)
+    # get metabase info
+    metabase_config = frappe.get_single('Metabase Settings')
+    # get dashboard info
+    dashboard = frappe.get_doc('Metabase Dashboard', dashboard)
 
-	# config token
-	payload = {
-		'resource': {'dashboard': int(dashboard.dashboard_id)},
-		'params': {},
-	}
-	# set expiration time
-	exp_time = metabase_config.metabase_exp_time
-	if exp_time:
-		payload['exp'] = round(time.time()) + (60 * exp_time)  # 60 second * minute
+    # config token
+    payload = {
+        'resource': {'dashboard': int(dashboard.dashboard_id)},
+        'params': {},
+    }
+    # set expiration time
+    exp_time = metabase_config.metabase_exp_time
+    if exp_time:
+        payload['exp'] = round(time.time()) + (60 * exp_time)  # 60 second * minute
 
-	# gen token
-	token = jwt.encode(
-		payload,
-		metabase_config.metabase_secret,
-		algorithm='HS256'
-	)
+    # gen token
+    token = jwt.encode(
+        payload,
+        metabase_config.metabase_secret,
+        algorithm='HS256'
+    )
 
-	# prepare config
-	config = []
-	if dashboard.show_border:
-		config.append('bordered=true')
-	else:
-		config.append('bordered=false')
-	if dashboard.show_title:
-		config.append('titled=true')
-	else:
-		config.append('titled=false')
-	if dashboard.theme == 'Dark':
-		config.append('theme=night')
-	config_param = '&'.join(config)
+    # prepare config
+    config = []
+    if dashboard.show_border:
+        config.append('bordered=true')
+    else:
+        config.append('bordered=false')
+    if dashboard.show_title:
+        config.append('titled=true')
+    else:
+        config.append('titled=false')
+    if dashboard.theme == 'Dark':
+        config.append('theme=night')
 
-	# prepare url
-	resizer = ''.join([
-		metabase_config.metabase_url,
-		'/app/iframeResizer.js',
-	])
-	iframeUrl = ''.join([
-		metabase_config.metabase_url,
-		'/embed/dashboard/',
-		token.decode('utf8'),
-		'#',
-		config_param,
-	])
+    #config.append('user='+frappe.session.user)
+    config_param = '&'.join(config)
 
-	return {
-		'name': dashboard.dashboard_name,
-		'resizer': resizer,
-		'iframeUrl': iframeUrl
-	}
+    # prepare url
+    resizer = ''.join([
+        metabase_config.metabase_url,
+        '/app/iframeResizer.js',
+    ])
+    iframeUrl = ''.join([
+        metabase_config.metabase_url,
+        '/embed/dashboard/',
+        token,
+#        '&user='+frappe.session.user,
+        '#',
+        config_param,
+    ])
+
+    #Rewriting meatebase iframeUrl generation here
+    METABASE_SITE_URL = metabase_config.metabase_url
+    METABASE_SECRET_KEY = metabase_config.metabase_secret
+
+    payload = {
+    "resource": {"dashboard": int(dashboard.dashboard_id)},
+    "params": {
+        "user": frappe.session.user
+    },
+    "exp": round(time.time()) + (60 * 10) # 10 minute expiration
+    }
+    token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
+    iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token + "#bordered=true&titled=true"    
+
+    return {
+        'name': dashboard.dashboard_name,
+        'resizer': resizer,
+        'iframeUrl': iframeUrl
+    }
---
+++ metabase_integration/metabase_integration/page/metabase_dashboard_view/metabase_dashboard_view.js
@@ -0,0 +1,125 @@
+// frappe.pages['metabase-dashboard-view'].on_page_load = function(wrapper) {
+// 	var page = frappe.ui.make_app_page({
+// 		parent: wrapper,
+// 		title: 'None',
+// 		single_column: true
+// 	});
+// }
+
+/* eslint require-jsdoc: 0 */
+
+frappe.pages['metabase-dashboard-view'].on_page_load = (wrapper) => {
+	// init page
+	const page = frappe.ui.make_app_page({
+		'parent': wrapper,
+		'title': 'Dashboard',
+		'single_column': true,
+	});
+
+	new MetabaseDashboard(page, wrapper);
+};
+
+class MetabaseDashboard {
+	constructor(page, wrapper) {
+		this.currentDashboard = false;
+		this.wrapper = wrapper;
+		this.pageMain = $(page.main);
+		this.pageAction = (
+			$(this.wrapper)
+				.find('div.page-head div.page-actions')
+		);
+		this.pageTitle = $(this.wrapper).find('div.title-text');
+
+		this.init();
+	}
+
+	init() {
+		this.createSelectionField();
+	}
+
+	showIframe() {
+		this.getSettings().then(
+			(r) => {
+				// set variable
+				this.settings = r.message;
+				this.resizer = this.settings.resizer;
+				this.iframeUrl = this.settings.iframeUrl;
+//				this.iframeUrl += "&user="+frappe.session.user;
+				this.name = this.settings.name;
+
+				if (this.iframeUrl && this.resizer) {
+					// prepare html
+					const iFrameHtml = `
+						<script id="resizer" src="${this.resizer}"></script>
+						<iframe
+							src="${this.iframeUrl}"
+							frameborder="0"
+							width=100%
+							onload="iFrameResize({}, this)"
+							allowtransparency
+						></iframe>
+					`;
+
+					// append html to page
+					this.iFrame = $(iFrameHtml).appendTo(this.pageMain);
+				}
+			}
+		);
+	}
+
+	getSettings() {
+		return frappe.call({
+			'method': 'metabase_integration.metabase_integration.doctype.metabase_dashboard.get_url',
+			'args': {
+				'dashboard': this.dashboardName,
+			},
+		});
+	}
+
+	createSelectionField() {
+		// create dashboard selection field
+		this.selectionField = frappe.ui.form.make_control({
+			'parent': this.pageAction,
+			'df': {
+				'fieldname': 'Dashboard',
+				'fieldtype': 'Link',
+				'options': 'Metabase Dashboard',
+				'onchange': () => {
+					const dashboardName = this.selectionField.get_value();
+					if (dashboardName) {
+						this.dashboardName = dashboardName;
+						if (this.currentDashboard != this.dashboardName) {
+							// clear page html
+							this.pageMain.empty();
+
+							this.showIframe();
+							this.changeTitle();
+
+							// set current dashboard
+							this.currentDashboard = this.dashboardName;
+						}
+						// clear input
+						this.selectionField.set_input('');
+					}
+				},
+				'get_query': () => {
+					return {
+						'filters': {
+							'is_active': 1,
+						},
+					};
+				},
+				'placeholder': 'Select Dashboard',
+			},
+			'render_input': true,
+		});
+
+		// change css
+		this.pageAction.removeClass('page-actions');
+		this.selectionField.$wrapper.css('text-align', 'left');
+	}
+
+	changeTitle() {
+		this.pageTitle.text(`${this.dashboardName} Dashboard`);
+	}
+}
